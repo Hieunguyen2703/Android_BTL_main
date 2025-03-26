7a23e9e2cf33194680d8a04b8fc50bbf468581f8
hieumn2703@gmail.com <Hieunguyen2703>
2025-03-26 22:11:17 +0700

    first commit

--- app/src/main/java/com/lapTrinhUUDD/movie/User/User.java
+++ app/src/main/java/com/lapTrinhUUDD/movie/User/User.java
@@ -1,23 +1,61 @@
 package com.lapTrinhUUDD.movie.User;
 
-public class User
-{
-    private String name;
-    private String password;
+public class User {
+    String username, email, dob ,gender, img;
 
-    public String getName() {
-        return name;
+    public User(String username, String email, String dob, String gender, String img) {
+        this.username = username;
+        this.email = email;
+        this.dob = dob;
+        this.gender = gender;
+        this.img = img;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public User() {
     }
 
-    public String getPassword() {
-        return password;
+    public User(String username, String email, String dob, String gender) {
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getImg() {
+        return img;
     }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
+
